5d8d94aad50960f4251e907a02cd362cb79eebb0
Olli-Pekka Pohjola <op@diory.me>
2024-03-16 20:39:20 +0200

    Export types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 export { Diosphere } from './diosphere/diosphere'
 export { Room } from './room/room'
-
-export { IDiosphere, IDiosphereObject, IRoomsObject, IRoom, IRoomObject, IRoomProps, IDoorObject, IConnectionObject } from './types'
