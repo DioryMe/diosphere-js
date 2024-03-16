d38cd143dc3391a758605f74495dc8670de6784d
Olli-Pekka Pohjola <op@diory.me>
2024-03-16 20:40:50 +0200

    chore: v0.2.5

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
 export { Diosphere } from './diosphere/diosphere'
 export { Room } from './room/room'
+
+export {
+  IDiosphere,
+  IDiosphereObject,
+  IRoomsObject,
+  IRoom,
+  IRoomObject,
+  IRoomProps,
+  IDoorObject,
+  IConnectionObject,
+} from './types'
